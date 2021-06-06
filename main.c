f55f8a8ff3e927ab854989b282c4e44a0ba78982
Lisa9502 <lisa.amalou.02@gmail.com>
2021-06-06 19:01:36 +0200

    Simplification du code

--- main.c
+++ main.c
@@ -6,17 +6,14 @@
 
 void afficherMat (char mat[LIGNES][COLONNES])
 {
-    char lettres[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
     printf(" ");
     for (int i = 0; i < COLONNES; i++)
-        printf(" %c", lettres[i]);
+        printf(" %c", 97+i);
     printf("\n");
 
     for(int i=0;i<LIGNES;i++)
     {
-        printf("%c", (lettres[i] - 32));
+        printf("%c", (97 - 32 + i));
         for(int j=0;j<COLONNES;j++)
         {
             mat[i][j]='1';
