3f50bcaf8396eec291fd095ac7222691879728e4
Lisa9502 <lisa.amalou.02@gmail.com>
2021-06-06 18:50:52 +0200

    Création matrice

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define LIGNES 12
+#define COLONNES 26
+
+
+void afficherMat (char mat[LIGNES][COLONNES])
+{
+    for(int i=0;i<LIGNES;i++)
+    {
+        for(int j=0;j<COLONNES;j++)
+        {
+            mat[i][j]='1';
+            printf("%c",mat[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main()
-{
-    printf("Hello world! modifie\n");
-    for (int i = 0; i < 10; i++)
-        printf("Wow a banana !");
-        printf("Winter is coming...");
-        printf("Brooklyn nine-nine");
+{   char mat[LIGNES][COLONNES];
+    afficherMat (mat);
     return 0;
 }
