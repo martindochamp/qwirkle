22941476605b2ea357cf6ba727003935af9d26ec
martindochamp <martinpaulcondet@gmail.com>
2021-06-06 18:55:47 +0200

    Alphabet

--- main.c
+++ main.c
@@ -6,12 +6,21 @@
 
 void afficherMat (char mat[LIGNES][COLONNES])
 {
+    char lettres[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    printf(" ");
+    for (int i = 0; i < COLONNES; i++)
+        printf(" %c", lettres[i]);
+    printf("\n");
+
     for(int i=0;i<LIGNES;i++)
     {
+        printf("%c", (lettres[i] - 32));
         for(int j=0;j<COLONNES;j++)
         {
             mat[i][j]='1';
-            printf("%c",mat[i][j]);
+            printf(" %c",mat[i][j]);
         }
         printf("\n");
     }
