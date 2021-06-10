bfa85a7c8f37278519df280b30102a33ae6be5e5
Lisa9502 <lisa.amalou.02@gmail.com>
2021-06-10 15:10:19 +0200

    Tuiles

--- tuiles.c
+++ tuiles.c
@@ -1,5 +1,26 @@
 #include "tuiles.h"
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void remplirStructure(t_tuile *tuile)
+{
+    tuile->couleur = 4;
+    tuile->forme = 0x03;
+    // {0x03, 0x04, 0x05, 0x06, 0x07, 0xDB,};
+}
+
+void afficherStructure(t_tuile* tuile)
+{
+    printf("%d", tuile->couleur);
+
+}
 
 void prgmTuiles() {
     printf("Wow ça marche !");
+    t_tuile tuile;
+    remplirStructure(&tuile);
+    afficherStructure(&tuile);
 }
+
+
--- tuiles.h
+++ tuiles.h
@@ -1,40 +1,16 @@
 #ifndef TUILES_H_INCLUDED
 #define TUILES_H_INCLUDED
-#include <windows.h>
 
 typedef struct Tuiles
 {
-    int couleur[6];
-    char forme[6];
-} t_Tuiles
+    int couleur;
+    char forme;
+} t_tuile;
 
-void remplirStructure(t_Tuiles *Tuiles )
-{
-    (*Tuiles).couleur[0]= {4};
-    (*Tuiles).forme[6]= {0x03, 0x04, 0x05, 0x06, 0x07, 0xDB,};
+void afficherStructure(t_tuile* tuile);
 
-    return t_Tuiles *Tuiles;
+void remplirStructure(t_tuile *tuile);
 
-}
-void afficherStructure (t_Tuiles Tuiles )
-{
-    printf("%s",(Tuiles).couleur);
-
-}
-
-
-int main() // La fonction "main"
-{
-    t_Tuiles Tuiles;
-    remplirStructure(&Tuiles);
-    afficherStructure (Tuiles);
-    return 0;
-}
-
-void Color(t_Tuiles *Tuiles )
-HANDLE H=GetStdHandle(STD_OUTPUT_HANDLE);
-SetConsoleTextAttribute(H,(*Tuiles).couleur);
-}
-
+void prgmTuiles();
 
 #endif // TUILES_H_INCLUDED
